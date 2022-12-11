4123d42de511b7e7ec14e0554d0a6d884d700bf9
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-12-11 13:45:24 +0100

    update prettier config

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
 
-const pucko = "barn";
+const pucko = 'barn';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return "Hello World!";
-  }
+	getHello(): string {
+		return 'Hello World!';
+	}
 }
