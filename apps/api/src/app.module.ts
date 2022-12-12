53b6e2545b0daa19d98f20e98ed2458506b3ab64
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-12-12 20:15:18 +0100

    remove app routes

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,10 @@
+// External dependencies
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+// Internal dependencies
+import { HealthModule } from './health/health.module';
 
 @Module({
-	imports: [],
-	controllers: [AppController],
-	providers: [AppService]
+	imports: [HealthModule]
 })
 export class AppModule {}
