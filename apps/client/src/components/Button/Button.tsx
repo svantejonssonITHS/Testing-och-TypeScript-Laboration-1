bc36c34b1ef5616895fe2d6b9e9eeacc07addb7f
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-12-20 10:59:26 +0100

    add disabled state to button

--- apps/client/src/components/Button/Button.tsx
+++ apps/client/src/components/Button/Button.tsx
@@ -5,13 +5,20 @@ interface ButtonProps {
 	children: JSX.Element | string;
 	onClick: () => void;
 	variant?: ButtonVariant;
+	disabled?: boolean;
 }
 
-export default function Button({ children, onClick, variant = ButtonVariant.FILL }: ButtonProps): JSX.Element {
+export default function Button({
+	children,
+	onClick,
+	variant = ButtonVariant.FILL,
+	disabled
+}: ButtonProps): JSX.Element {
 	return (
 		<button
 			className={[style['button'], style[variant]].join(' ')}
 			onClick={onClick}
+			disabled={disabled}
 		>
 			{children}
 		</button>
